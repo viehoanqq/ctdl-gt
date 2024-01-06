77aa81f2e96884d8d917a403e549621f30a02440
Việt Hoàng <144252161+viehoanqq@users.noreply.github.com>
2024-01-07 02:28:11 +0700

    Add files via upload

--- SearchingAlgorithm.cpp
+++ SearchingAlgorithm.cpp
@@ -1,38 +1,64 @@
 #include <iostream>
 using namespace std;
-int search(int arr[],int n,int val)
-{
-	for (int i=0;i<n;i++)
-	{
-		if ( arr[i] == val)
-		return i;
-	}
-	return -1;
-}
+class search{
+    private :
+        int n;
+        int arr[10];
+    public:
+        void nhap(){
+            cout<<"nhap: ";
+            cin>>n;
+            for (int i=0;i<n;i++){
+                cin>>arr[i];
+            }
+        }
+        void sort(){
+            for (int i=0;i<n-1;i++)
+            for (int j=i;j<n;j++)
+            if (arr[i] > arr[j]){
+                swap(arr[i],arr[j]);
+            }
+        }
+        int count(){
+            return n;
+        }
+        
+        int linearSearch(){
+            int a;
+            cin>>a;
+            for (int i=0;i<n;i++)
+            if (arr[i] == a)
+                return i;
+            return -1;
+        }
+        
+        int  binarySearch(int l,int r){
+            int a;
+            cin>>a;
+            while (l<=r){
+                int mid = l+(r-l)/2;
+                if (arr[mid] == a) return mid;
+                else if (a > mid)
+                        l=mid+1;
+                    else 
+                        r=mid-1;
+            }
+            return -1;
+            
+            
+        }
+        int binarySearch2(int l,int r,int target){
+            if (l > r ) return -1;
+            int mid = l + (r-l)/2;
+            if (arr[mid] == target) return mid;
+            if (target < mid) return binarySearch2(l,mid-1,target);
+            else return binarySearch2(mid+1,r,target);
+        }
+};
 
-int BinarySearch(int arr[],int l,int r,int val)
-{
-	if ( r>=l){
-
-	int mid= l+(r-l)/2;
-	if ( arr[mid] == val) return mid;
-	if (arr[mid] > val) return BinarySearch(arr,l,mid-1,val);
- 	return BinarySearch(arr,mid+1,r,val);	
-	}
-	return -1;
-}
-void nhap(int arr[],int &n)
-{
-	cout<<"n = "; cin>>n;
-	for (int i=0;i<n;i++)
-	{
-		cin>>arr[i];
-	}
-}
-int main()
-{
-	int arr[100],n;
-	nhap(arr,n);
-	//cout<<search(arr,n,10);
-	cout<<endl<<BinarySearch(arr,0,n-1,3);
+int main(){
+    search test;
+    test.nhap();
+    test.sort(); // vi thuat toan tim kiem nhi phan can phai dung mang da sap xep
+    cout<<test.binarySearch2(0,test.count()-1,3);
 }
