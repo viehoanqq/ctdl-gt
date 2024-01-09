92a4a5ff88756fdd99fbf4c26c6a04d871fe925b
Việt Hoàng <144252161+viehoanqq@users.noreply.github.com>
2024-01-09 23:00:45 +0700

    Update BinarySearchTree.cpp

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -26,6 +26,11 @@ class BinarySearchTree{
         int findMax(Node * node);
         int findMin(Node * node);
         Node * remove(Node * node, int val);
+        int findLe(Node * node);
+        int count(Node * node);
+        int countLeaf(Node * node);
+        int count1nhanh(Node * node);
+        int sum(Node * node);
     public:
         void insert(int val);
         void printTreeInOrder();
@@ -35,9 +40,72 @@ class BinarySearchTree{
         void remove(int val);
         void printTreePostOrder();
         void printTreePreOrder();
+        int findLe();
+        int count();
+        int countLeaf();
+        int count1nhanh();
+        int sum();
 };
+int BinarySearchTree::sum(Node * node){
+    if (node ==NULL) return 0;
+    int s=node->value;
+    s+= sum(node->left);
+    s+=sum(node->right);
+    return s;
+}
+int  BinarySearchTree::sum(){
+    return sum(root);
+}
+int BinarySearchTree::count1nhanh(Node * node){
+    if (node == NULL) return 0;
+    int dem=0;
+    if ((node->right == NULL && node->left != NULL) || (node->right !=NULL && node->left==NULL)){
+        return 1;
+    }
+    dem+= count1nhanh(node->left);
+    dem+= count1nhanh(node->right);
+    return dem;
+}
 
+int BinarySearchTree::count1nhanh(){
+    return count1nhanh(root);
+}
+int BinarySearchTree::countLeaf(Node * node){
+    if (node == NULL ) return 0;
+    int dem=0;
+    if (node->right ==NULL && node->left == NULL){
+        return 1;
+    }
+    dem+=countLeaf(node->left);
+    dem+=countLeaf(node->right);
+    return dem;
+    
+}
 
+int BinarySearchTree::countLeaf(){
+    return countLeaf(root);
+}
+int BinarySearchTree::count(Node * node){
+    if (node == NULL) return 0;
+    int dem=1;
+    dem += count(node->left);
+    dem+= count(node->right);
+    return dem;
+}
+int BinarySearchTree::count(){
+    return count(root);
+}
+int BinarySearchTree::findLe(Node * node){
+    if (node == NULL) return 0;
+    int count =0;
+    if (node->value % 2 != 0) count++;
+    count += findLe(node->left);
+    count += findLe(node->right);
+    return count;
+}
+int BinarySearchTree::findLe(){
+    return findLe(root);
+}
 //----- protected :: code  ------ //
     Node * BinarySearchTree::insert(Node * node ,int val){
         Node * currentNode = new Node(val);
@@ -102,6 +170,39 @@ class BinarySearchTree{
         return findMin(node->left);
     }
     
+    Node * BinarySearchTree::remove(Node * node, int val) {
+    if (node == NULL) {
+        return node;
+    }
+
+    if (val < node->value) {
+        node->left = remove(node->left, val);
+    } else if (val > node->value) {
+        node->right = remove(node->right, val);
+    } else {
+        // Case 1: No child or only one child
+        if (node->left == NULL) {
+            Node *temp = node->right;
+            delete node;
+            return temp;
+        } else if (node->right == NULL) {
+            Node *temp = node->left;
+            delete node;
+            return temp;
+        }
+
+        // Case 2: Two children
+        Node *minRightSubtree = node->right;
+        while (minRightSubtree->left != NULL) {
+            minRightSubtree = minRightSubtree->left;
+        }
+
+        node->value = minRightSubtree->value;
+        node->right = remove(node->right, minRightSubtree->value);
+    }
+
+    return node;
+}
 //----- publlic :: code  ------ //
     void BinarySearchTree::insert(int val){
         root = insert(root,val);
@@ -127,6 +228,9 @@ class BinarySearchTree{
     int BinarySearchTree::findMin(){
         return findMin(root);
     }
+    void BinarySearchTree::remove(int val){
+        root = remove(root,val);
+    }
     
 int main(){
     BinarySearchTree bst;
@@ -146,7 +250,14 @@ int main(){
     cout<<" search 48: " <<bst.search(48); cout<<endl;
     cout<<" findMax : " <<bst.findMax(); cout<<endl;
     cout<<" findMin : " <<bst.findMin(); cout<<endl;
+    cout<<"remove 56 : ";bst.remove(16);cout<<endl;
+    cout<<"find le: "<<bst.findLe();cout<<endl;
+    bst.printTreeInOrder();
+    cout<<" count : "<<bst.count();cout<<endl;
     
+    cout<<"count of leaf : "<<bst.countLeaf();cout<<endl;
+    cout<<"count 1 nhanh : "<<bst.count1nhanh();cout<<endl;
+    cout<<"sum = "<<bst.sum();
 }
     
     
